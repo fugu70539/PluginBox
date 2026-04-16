7a46e158d61dc55442481bd106471cc7351e7590
fugu70539 <fugu70539@gmail.com>
2026-04-16 19:40:05 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
+import Script from "next/script";
 import "./globals.css";
-
-export const metadata = {
-  title: "PluginBox",
-  description: "Next-gen Telegram Mini App",
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
+    <html lang="ru">
+      <head>
+        <Script 
+          src="https://telegram.org/js/telegram-web-app.js" 
+          strategy="beforeInteractive" 
+        />
+      </head>
+      <body className="antialiased bg-black">{children}</body>
     </html>
   );
 }
