82b0c3f156307a538527926773405102acfb3a22
fugu70539 <fugu70539@gmail.com>
2026-04-16 12:24:39 +0300

    Create Socket.tsx

---
+++ src/components/views/Socket.tsx
@@ -0,0 +1,9 @@
+export default function Socket() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black px-10">
+      <div className="w-full glass-effect bg-white/5 border border-white/10 p-10 rounded-ios text-center">
+        <span className="text-white/20 font-mono text-[10px]">SYSTEM_SOCKET_OFFLINE</span>
+      </div>
+    </div>
+  );
+}
