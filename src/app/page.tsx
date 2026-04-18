fa09ea39c26b7ad0f20c24f231e6293443eb70a9
fugu70539 <fugu70539@gmail.com>
2026-04-18 18:37:02 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Hub from "@/components/views/Hub";
 import Store from "@/components/views/Store";
 import Socket from "@/components/views/Socket";
-import { Tabbar } from "../components/ui/Tabbar";
+import { Tabbar } from "@/components/ui/TabBar";
 
 export default function Page() {
   const [activeTab, setActiveTab] = useState("hub");
