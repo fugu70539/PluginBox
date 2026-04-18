17cb8db58e4816ff2a29fb5d69d75c9d978208fa
fugu70539 <fugu70539@gmail.com>
2026-04-18 17:11:19 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script"; // Импортируем компонент для скриптов
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,6 +22,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className="fixed-layout">
+      <head>
+        {/* Подключаем основной скрипт Telegram API */}
+        <Script 
+          src="https://telegram.org/js/telegram-web-app.js" 
+          strategy="beforeInteractive" 
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
