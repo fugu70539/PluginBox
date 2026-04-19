a501957bca9aac36ae5ca16321d5ea1e725453be
fugu70539 <fugu70539@gmail.com>
2026-04-19 11:11:29 +0300

    Create Developers.tsx

---
+++ src/components/views/Developers.tsx
@@ -0,0 +1,7 @@
+export default function Developers() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-52 opacity-20">
+      <p className="text-[14px] font-bold uppercase tracking-[0.2em]">Раздел в разработке</p>
+    </div>
+  );
+}
